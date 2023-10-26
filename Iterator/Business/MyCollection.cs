b65f8047daad68eb57792ef6a5fb2e8428e7946e
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-26 17:43:38 +0300

    Iterator implamantation.

---
+++ Iterator/Business/MyCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.Business;
+
+public class MyCollection : IEnumerable<int>
+{
+    private List<int> items = new List<int>();
+
+    public void Add(int item)
+    {
+        items.Add(item);
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,10 @@
+﻿using Iterator.Business;
+
+MyCollection collection = new MyCollection();
+
+collection.Add(1);
+collection.Add(2);
+collection.Add(3);
+
+foreach (int item in collection)
+    Console.WriteLine(item);
